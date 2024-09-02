1bdcff5f4dac5ee43eb585e126deff11ab3c8583
Indranil Saha <141521650+indranilsaha2003@users.noreply.github.com>
2024-09-02 15:28:04 +0530

    Update Footer.jsx

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -1,17 +1,24 @@
 import React from "react";
-import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
     <footer id="contact" className="footer footer-three">
       <div className="footer-container">
-        <p className="text-center">Some text in the center</p>
+        <p className="text-center">: Quick links :</p>
         <div className="d-flex justify-content-center">
-          <a href="#about" className="mx-2">About</a>
-          <a href="#contact" className="mx-2">Contact</a>
-          <Link to='../pages/Login'>Login</Link>
+          <a href="#" className="mx-2">
+            About
+          </a>
+          <a href="#" className="mx-2">
+            Contact
+          </a>
+          <a href="#" className="mx-2">
+            Login
+          </a>
         </div>
-        <p className="text-center mt-2">&copy; Made with love by Team Botcoders</p>
+        <p className="text-center mt-2">
+          &copy; Made with love by Team Botcoders
+        </p>
       </div>
     </footer>
   );
